5badab42dd5fbd495d615e38084b14a7e014b73a
Shafin <shafin.ahmed.1512023@gmail.com>
2023-11-06 23:05:23 +0600

    NSMSA First Launched 🥰

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <div className="landing">
+                <div className="hero-text">
+                    <h1>NSMSA</h1>
+                    <h3>
+                        Welcome to a <span>safe world</span>
+                    </h3>
+                </div>
+
+                <button>Explore</button>
+            </div>
+        </>
+    );
+};
+
+export default App;
